6f0394adc28dd676aeb2fd4ab0c8a085dd60a879
vladimir.bareisha <vladimir.bareisha@vizor-games.com>
2022-07-05 12:57:18 +0300

    Add task with comments

--- Program.cs
+++ Program.cs
@@ -1,99 +1,132 @@
 ﻿class Program
 {
-    
-
     static void Main()
     {
+        /**
+          Для начала можно убрать твои 10 строк где ты ставишь 0.
+          Если ты обьявляешь массив или матрицу из простых типов
+          к примеру int то они по умолчанию уже = 0.
+          r1 непонятное имя для поля, давай назовем 
+        **/
+        int[,] battleGround = new int[10, 10];
 
-
-        int[,] r1 = new int[10, 10] { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 } };
-
-        Random rnd = new Random();
+        /**
+          Я бы назвал переменную random, не экономь места пиши длинные понятные названия
+        **/
+        Random random = new Random();
         
-
-        
+        // однопалубные
         int ship = 1;
         while (ship < 5)
         {
-            int g = rnd.Next(0, 10);
-            int g2 = rnd.Next(0, 10);
+            int i = random.Next(0, 10);
+            int j = random.Next(0, 10);
 
-            if (r1[g, g2] == 0) 
+            if (battleGround[i, j] == 0) 
             {
-                r1[g, g2] = 1;
-
+                battleGround[i, j] = 1;
                 ship++;
             }
             else
             {
                 continue;
             }
-
-            
-
         }
-        //Ship 2
 
+        //Двух палубные
         int ship2 = 1;
         while (ship2 < 4)
         {
-            int g = rnd.Next(0, 10);
-            int g2 = rnd.Next(0, 10);
+            int i = random.Next(0, 10);
+            int j = random.Next(0, 10);
 
-            if (r1[g, g2] == 0)
+            /**
+                1. Найти свободную клетку
+                2. Определить в какие 4 стороны будет корабль расти up, down, left, right
+                3. Проверить влазит ли по координатам корабль в 2 клетки и не пересекается с другими кораблями
+                4. Если влазит, то размещаем все палубы, иначе continue
+            **/
+            if (battleGround[i, j] == 0) //  1. Найти свободную клетку
             {
-                r1[g, g2] = 2;
-                
-                //ship2 [q] = [g, g2]; //попытка хороша
+                /**
+                  0 - up
+                  1 - down
+                  2 - left
+                  3 - right
+                **/
+                // 2. Определить в какие 4 стороны будет корабль расти up, down, left, right
+                int direction = random.Next(0, 4);
+                if (direction == 0) // up
+                {
+                    //  3. Проверить влазит ли по координатам корабль в 2 клетки и не пересекается с другими кораблями
+                    if (i - 1 >= 0 && battleGround[i - 1, j] == 0)
+                    {
 
-                
-                
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (direction == 1) // down
+                {
+                    //  3. Проверить влазит ли по координатам корабль в 2 клетки и не пересекается с другими кораблями
+                    if (i + 1 < 10 && battleGround[i + 1, j] == 0)
+                    {
 
-                ship2++;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (direction == 2) // left
+                {
+                    //  3. Проверить влазит ли по координатам корабль в 2 клетки и не пересекается с другими кораблями
+                    if (j - 1 >= 0 && battleGround[i, j - 1] == 0)
+                    {
+
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (direction == 3) // right
+                {
+                    //  3. Проверить влазит ли по координатам корабль в 2 клетки и не пересекается с другими кораблями
+                    if (j + 1 < 10  && battleGround[i, j + 1] == 0)
+                    {
+
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
             }
             else
             {
                 continue;
             }
-            foreach (int i in r1)
-                if (i == 2)
-                { 
-                    for (int e = 1; e < r1.GetLength(1); e++)
-                    {
-                        if (e <= r1.GetLength(1))
-                        {
-                            //r1[g + e, g2] = 2;
-                        }
-                    }
-                    r1[i + 11, g2] = 2;
-                }
-            //
-            //for (int i = 1; i <r1.Get.Lenght(1); i++)
-            //{
-            //    if (i <= r1.GetLength(1))
-            //    {
-            //        r1[g + i, g2] = 2;
-            //    }
-            //}
-
-            //
         }
+        
+        /**
+          Для читабельности я вынес твой код по распечатке поля в отдельный метод.
+          В метод передается матрица, которую надо распечатать
+        **/
+        printBattleGround(battleGround);
+    }
 
-
-        for (int i = 0; i < r1.GetLength(0); i++)
+    private static void printBattleGround(int [,] battleGround)
+    {
+        for (int i = 0; i < 10; i++)
         {
-
-            for (int j = 0; j < r1.GetLength(1); j++)
+            for (int j = 0; j < 10; j++)
             {
-                Console.Write($"{r1[i, j]} ");
-
-                
+                Console.Write($"{battleGround[i, j]} ");
             }
             Console.WriteLine();
         }
-
     }
-
 }
-
-        
